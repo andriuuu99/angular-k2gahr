7d2e1c1542ce1e3eb1f184024f9904e095da25fa
andriuuu99 <andresrodriguez96565@gmail.com>
2022-01-06 18:10:30 +0100

    1 file modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -11,10 +11,7 @@ import { CartService } from '../cart.service';
 })
 export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
-  constructor(
-    private route: ActivatedRoute,
-    private cartService: CartService
-  ) {}
+  constructor(private route: ActivatedRoute) {}
 
   /* ...
   addToCart(product: Product) {
